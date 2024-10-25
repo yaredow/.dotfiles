7609cba3e0fa59e0b1663a7420296b302b4a00a1
yaredow <yaredyilma11@gmail.com>
2024-10-25 11:46:03 +0300

    configure the colorscheme and add it to lualine and telescope

--- .config/nvim/lua/yada/plugins/colorscheme.lua
+++ .config/nvim/lua/yada/plugins/colorscheme.lua
@@ -1,10 +1,21 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha", -- Can be "latte", "frappe", "macchiato", or "mocha"
-    })
-    vim.cmd.colorscheme("catppuccin")
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha", -- Can be "latte", "frappe", "macchiato", or "mocha"
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = false,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
