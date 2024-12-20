f7e49389a8bc181c9e57ab4da77840e710aa9876
yaredow <yaredyilma11@gmail.com>
2024-12-20 02:40:49 +0000

    replace nvim-tree with neo-tree

--- .config/nvim/lua/yada/plugins/blade.lua
+++
@@ -1,6 +0,0 @@
-return {
-	{
-		"jwalton512/vim-blade",
-		ft = "blade", -- Load only for Blade files
-	},
-}
---
+++ .config/nvim/lua/yada/plugins/copilot.lua
@@ -0,0 +1,7 @@
+return {
+	"github/copilot.vim",
+	cmd = "Copilot",
+	config = function()
+		vim.cmd("Copilot enable")
+	end,
+}
