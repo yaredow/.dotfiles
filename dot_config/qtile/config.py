9a342e275d5b4bfc85d28c2d7e93862769d0ea34
yaredow <yaredyilma11@gmail.com>
2025-07-29 11:25:08 +0000

    add remaining dot files

---
+++ dot_config/qtile/config.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+from libqtile import layout, hook, qtile
+from libqtile.config import Match
+from typing import List  # noqa: F401
+
+# Custom configuration imported from configs folder
+from configs.groups import groups
+from configs.screens import screens
+from configs.layouts import layouts
+from configs.keys import keys
+
+keys = keys
+
+border = dict(border_focus="#aaeedd", border_normal="#4c566a")
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(
+    border_width=2,
+    **border,
+    float_rules=[
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="Tk"),  # ssh-askpass
+        Match(wm_class="Signal Beta"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+        Match(wm_class="chromium", role="pop-up"), # Google Chrome picture-in-picture
+    ],
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+auto_minimize = True
+wl_input_rules = None
+wmname = "LG3D"
+
+# Script for displays position, always execute if you reload QTILE config
+@hook.subscribe.startup
+@hook.subscribe.startup_once
+def start_once():
+    home = os.path.expanduser("~")
+    subprocess.call([home + "/.config/qtile/autostart.sh"])
