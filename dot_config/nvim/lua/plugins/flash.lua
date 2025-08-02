98abc316bb031f90230db07f1f12b6a99fc951a2
yaredow <yaredyilma11@gmail.com>
2025-08-02 16:02:41 +0300

    finish configuring astronvim

--- dot_config/nvim/lua/plugins/flash.lua
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -2,13 +2,7 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {
-    modes = {
-      search = {
-        enabled = true
-      },
-    },
-  },
+  opts = {},
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
